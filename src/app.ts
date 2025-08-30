2b410deddbe942635328cf8b4cc6f2f928b784a5
Politologod <hector.a.briceno@ucv.ve>
2025-08-30 05:43:41 -0400

    Agrego la configuracion de express y ESModule

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+// src/index.ts
+
+import express, { type Express, type Request, type Response } from 'express';
+
+// Crea una instancia de la aplicación Express
+const app: Express = express();
+const port: number = 3000;
+
+// Middleware para parsear JSON (opcional pero común)
+app.use(express.json());
+
+// Ruta de ejemplo
+app.get('/', (req: Request, res: Response) => {
+  res.send('¡Hola Mundo!');
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(` Servidor corriendo en http://localhost:${port}`);
+});
