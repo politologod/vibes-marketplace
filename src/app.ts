dddec991926b6ad1372d1db5088353a493e1bcde
Politologod <hector.a.briceno@ucv.ve>
2025-08-31 04:01:19 -0400

    Se agrego un payload a app.ts sin intencion mientras se hacia QA probando el registro desde postman, arreglo de app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,7 @@ const app: Express = express();
 const port: number = 3000;
 
 app.use(cors({
-  origin: ['http://localhost:3000{
-  "email": "maria.gonzalez@hotmail.com",
-  "password": "password456",
-  "nombreCompleto": "María Alejandra González",
-  "cedula": "V-87654321",
-  "numeroTelefono": "+58412-9876543",
-  "direccion": "Calle 15, Maracaibo, Zulia"
-}
+  origin: ['http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
