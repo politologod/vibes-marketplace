863ec6bfd2034eeaba0c18ba8333ed0a90a7a693
Hector Briceño <hector.a.briceno@ucv.ve>
2025-09-01 09:17:37 -0400

    fixes(api): colocando la url de produccion en CORS

--- api/src/app.ts
+++ api/src/app.ts
@@ -13,7 +13,7 @@ const app: Express = express();
 const port: number = 3001;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://vibes-marketplace.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
